aaa6f7e6c47b2bb230ceed684606898ab258180d
Kazuki Endo <s1180047@gmail.com>
2014-04-13 17:46:01 +0900

    First Commit

---
+++ Assets/BallScript.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+var FirstForce:Vector3 = Vector3(300, 0, 300);
+function Start () {
+	rigidbody.AddForce(FirstForce);
+}
+
+function Update () {
+	if(transform.position.z < 0){
+		Debug.Log("Game Over!!");
+		Application.LoadLevel("Main");
+	}
+}
+
+function OnCollisionEnter(obj : Collision){
+	audio.Play();
+	if(obj.gameObject.tag == "Block"){
+		Destroy(obj.gameObject);
+	}
+
+	// バウンド時の加速度制御
+	rigidbody.velocity = rigidbody.velocity.normalized * 10;
+	if (Mathf.Abs(rigidbody.velocity.z) < 3) {
+		rigidbody.velocity.z = rigidbody.velocity.z * 3;
+	}
+	if (Mathf.Abs(rigidbody.velocity.x) < 3) {
+		rigidbody.velocity.x = rigidbody.velocity.x * 3;
+	}
+}
---
+++ Assets/PlayerScript.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+var x : double = 0.0;
+
+function Update () {
+
+	x = Input.GetAxis("Horizontal");
+	transform.Translate(x * 0.1, 0, 0);
+
+}
